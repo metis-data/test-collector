6a0049aa0c9890bd88dcd4228f69c13b6f531a1e
Issackr <issackr@metisdata.io>
2024-01-25 14:28:41 +0200

    refactor action

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core';
+import { context } from '@actions/github';
+import { setupCollector } from './utils';
+
+const apiKey = core.getInput('metis-api-key');
+const connectionString = core.getInput('connection-string');
+const logLevel = core.getInput('metis-log-level');
+const exporterTargetUrl = core.getInput('exporter-target-url');
+const shell = core.getInput('shell');
+const network = core.getInput('network');
+
+const { pull_request: pr } = context.payload;
+
+const appTagPr = pr
+  ? pr.title
+    ? `pr:${pr.title}`
+    : `pr:${context.sha}`
+  : `commit:${context.sha}`;
+
+const collectorId = setupCollector(
+  network,
+  connectionString,
+  apiKey,
+  exporterTargetUrl,
+  appTagPr,
+  logLevel,
+  { shell },
+);
+
+core.saveState('collector-id', collectorId);
+console.log('Metis otel collector is up and running...');
